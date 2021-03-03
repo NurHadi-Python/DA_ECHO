09ede38563050a6add627d92dd704df7e2f5382b
mahshao <78840617+mahshao@users.noreply.github.com>
2021-03-03 12:14:38 +0800

    Add files via upload

--- DA_ECHO.py
+++ DA_ECHO.py
@@ -30,12 +30,12 @@ print(rh.text)
 
 import scrapy
 class NewSpider(scrapy.Spider):
-    name = "new_spider"
+    name = "Project"
     start_urls = ["http://172.18.58.238/"]
     def parse(self, response):
         css_selector = 'img'
         for x in response.css(css_selector):
             newsel = '@src'
             yield {
-                'image Link': x.xpath(newsel).extract_first(),
+                'Image Link': x.xpath(newsel).extract_first(),
             }
